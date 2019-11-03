840b7dbc2312e8c24231d0726e9b4f9a68cd3291
Chris Haining <chrishaining@yahoo.co.uk>
2019-11-03 20:03:20 +0000

    deletes unnecessary text from App.js

--- news_client/src/App.js
+++ news_client/src/App.js
@@ -5,7 +5,6 @@ import NewsContainer from './containers/NewsContainer';
 function App() {
   return (
     <div className="App">
-      <h1>Guten Tag: Das is der App</h1>
       <NewsContainer />
     </div>
   );
