40e2cace7840c78f8c38b502867f34979e14982b
Chris Haining <chrishaining@yahoo.co.uk>
2019-11-03 18:11:36 +0000

    initial commit of server:

---
+++ NewsServer/src/main/java/com/codeclan/practiceNewsApp/PracticeNewsAppApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.practiceNewsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeNewsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeNewsAppApplication.class, args);
+	}
+
+}
---
+++ NewsServer/src/test/java/com/codeclan/practiceNewsApp/PracticeNewsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.practiceNewsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeNewsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
