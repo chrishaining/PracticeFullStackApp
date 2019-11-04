1f651b0ccd4aed243345b1ab5cc1e31d0f7dbbc3
Chris Haining <chrishaining@yahoo.co.uk>
2019-11-04 21:06:59 +0000

    front end can now pull data from backend and display it

--- news_client/src/containers/NewsContainer.js
+++ news_client/src/containers/NewsContainer.js
@@ -7,59 +7,77 @@ class NewsContainer extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            articles: [
-                {
-                    id: 1,
-                    date: "31/10/2019",
-                    headline: "Nuns in shoot-em-up drama",
-                    summary: "Some nuns were involved in a video game incident.",
-                    story: "Yesterday in Kuala Lumpur, some nuns on vacation from Lima were caught playing a violent video game.",
-                    journalist: "Nelly Mango"
-                },
-                {
-                    id: 2,
-                    date: "01/11/2019",
-                    headline: "Soup-reme Court goes pot-ty!",
-                    summary: "USA's top judges ban soup.",
-                    story: "The Supreme Court today banned all forms of soup, following a spate of soup-related crime. Since July, there have been 43 separate soup-related crimes across the United States. It all began with the Battle of Minestrone in Seattle, in which 12 people were killed. According to a government spokesperson, 'America has declared a war on soup. Soup is destroying our communities, and today's decision will lead to a safer world.",
-                    journalist: "Ishmael Kantosa"
-                }
-            ],
-            journalists: [
-                {
-                    id: 1,
-                    firstName: "Nelly",
-                    lastName: "Mango",
-                    phoneNumber: "7890123",
-                    articles: [{
-                        id: 1,
-                        date: "31/10/2019",
-                        headline: "Nuns in shoot-em-up drama",
-                        summary: "Some nuns were involved in a video game incident.",
-                        story: "Yesterday in Kuala Lumpur, some nuns on vacation from Lima were caught playing a violent video game.",
-                        journalist: "Nelly Mango"
-                    }]
-                },
+            articles: [],
+                // {
+                //     id: 1,
+                //     date: "31/10/2019",
+                //     headline: "Nuns in shoot-em-up drama",
+                //     summary: "Some nuns were involved in a video game incident.",
+                //     story: "Yesterday in Kuala Lumpur, some nuns on vacation from Lima were caught playing a violent video game.",
+                //     journalist: "Nelly Mango"
+                // },
+                // {
+                //     id: 2,
+                //     date: "01/11/2019",
+                //     headline: "Soup-reme Court goes pot-ty!",
+                //     summary: "USA's top judges ban soup.",
+                //     story: "The Supreme Court today banned all forms of soup, following a spate of soup-related crime. Since July, there have been 43 separate soup-related crimes across the United States. It all began with the Battle of Minestrone in Seattle, in which 12 people were killed. According to a government spokesperson, 'America has declared a war on soup. Soup is destroying our communities, and today's decision will lead to a safer world.",
+                //     journalist: "Ishmael Kantosa"
+                // }
+            // ],
+            journalists: []
+                // {
+                //     id: 1,
+                //     firstName: "Nelly",
+                //     lastName: "Mango",
+                //     phoneNumber: "7890123",
+                //     articles: [{
+                //         id: 1,
+                //         date: "31/10/2019",
+                //         headline: "Nuns in shoot-em-up drama",
+                //         summary: "Some nuns were involved in a video game incident.",
+                //         story: "Yesterday in Kuala Lumpur, some nuns on vacation from Lima were caught playing a violent video game.",
+                //         journalist: "Nelly Mango"
+                //     }]
+                // },
 
-                {
-                    id: 2,
-                    firstName: "Ishmael",
-                    lastName: "Kantosa",
-                    phoneNumber: "0123456",
-                    articles: [{
-                        id: 2,
-                        date: "01/11/2019",
-                        headline: "Soup-reme Court goes pot-ty!",
-                        summary: "USA's top judges ban soup.",
-                        story: "The Supreme Court today banned all forms of soup, following a spate of soup-related crime. Since July, there have been 43 separate soup-related crimes across the United States. It all began with the Battle of Minestrone in Seattle, in which 12 people were killed. According to a government spokesperson, 'America has declared a war on soup. Soup is destroying our communities, and today's decision will lead to a safer world.",
-                        journalist: "Ishmael Kantosa"
-                    }]
-                }
-            ]
-
+                // {
+                //     id: 2,
+                //     firstName: "Ishmael",
+                //     lastName: "Kantosa",
+                //     phoneNumber: "0123456",
+                //     articles: [{
+                //         id: 2,
+                //         date: "01/11/2019",
+                //         headline: "Soup-reme Court goes pot-ty!",
+                //         summary: "USA's top judges ban soup.",
+                //         story: "The Supreme Court today banned all forms of soup, following a spate of soup-related crime. Since July, there have been 43 separate soup-related crimes across the United States. It all began with the Battle of Minestrone in Seattle, in which 12 people were killed. According to a government spokesperson, 'America has declared a war on soup. Soup is destroying our communities, and today's decision will lead to a safer world.",
+                //         journalist: "Ishmael Kantosa"
+                //     }]
+                // }
+            // ]
 
         }
     }
+
+    componentDidMount(){
+    
+        const promises = [
+        fetch('http://localhost:8080/articles')
+        .then(res => res.json())
+        .then(res => res["_embedded"])
+        .then(res => res.articles)
+        .then(data => this.setState({articles: data}))
+          ,
+        fetch("http://localhost:8080/journalists")
+        .then(res => res.json())
+        .then(res => res["_embedded"])
+        .then(res => res.journalists)
+        .then(data => this.setState({journalists: data}))
+        ]
+        Promise.all(promises)
+      }
+
 
     render() {
         return (
